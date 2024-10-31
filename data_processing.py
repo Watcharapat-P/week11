42c6673262e6aa9bef367f7cb78580c1eda1c991
Watcharapat PATHANUTPONG <watcharapat.p@ku.th>
2024-10-31 15:38:46 +0700

    Implement function to data_processing.py (Procedural Style)

--- data_processing.py
+++ data_processing.py
@@ -35,20 +35,46 @@ print()
 
 # Print the average temperature for all the cities in Italy
 # Write code for me
+temp = []
+for city in cities:
+    if city['country'] == my_country:
+        temp.append(float(city['temperature']))
+print(temp)
+print(f'Average temperature : {sum(temp)/len(temp):.2f}')
 
 # Print the max temperature for all the cities in Italy
 # Write code for me
+print(f'Max temperature : {max(temp):.2f}')
+
 
 # Let's write a function to filter out only items that meet the condition
 def filter(condition, dict_list):
-    pass
+    filtered_list = []
+    for item in dict_list:
+        if condition(item):
+            filtered_list.append(item)
+    return filtered_list
+
+
+x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
+for item in x:
+    print(item)
+
 
 # Let's write a function to do aggregation given an aggregation function and an aggregation key
 def aggregate(aggregation_key, aggregation_function, dict_list):
-    pass
+    temp = []
+    for items in dict_list:
+        temp.append(float(items[aggregation_key]))
+    return aggregation_function(temp)
+
 
 # Let's write code to
 # - print the average temperature for all the cities in Italy
+print(f'Average temperature in Italy : {aggregate('temperature', lambda x: sum(x)/len(x), filter(lambda x: x['country'] == 'Italy', cities)):.2f}')
 # - print the average temperature for all the cities in Sweden
+print(f'Average temperature in Sweden : {aggregate('temperature', lambda x: sum(x)/len(x), filter(lambda x: x['country'] == 'Sweden', cities)):.2f}')
 # - print the min temperature for all the cities in Italy
+print(f'Minimum temperature in Italy : {aggregate('temperature', lambda x: min(x), filter(lambda x: x['country'] == 'Italy', cities)):.2f}')
 # - print the max temperature for all the cities in Sweden
+print(f'Max temperature in Italy : {aggregate('temperature', lambda x: max(x), filter(lambda x: x['country'] == 'Sweden', cities)):.2f}')
